5f6b84d070c26ad4827a591238386926d59b8f62
Kunal Gupta <167274095+kunalgupta21@users.noreply.github.com>
2026-01-12 06:14:48 +0530

    Remove commented-out routes and unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
 import "./App.css";
 import React from "react";
-//import { Button, Space } from 'antd';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import Login from "./pages/login";
 import Register from "./pages/register";
-// import Home from "./pages/home";
 import Template from "./pages/template";
 import Profile from "./pages/profile";
 import Templates from "./pages/templates";
 import Landing from "./pages/landing";
 import Upload from "./pages/upload";
 import Result from "./pages/result";
-import EnableMFA from "./pages/EnableMFA";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
-          {/* <Route path="/home" element={<Home />} /> */}
-          {/* <Route path="/landing" element={<Landing />} /> */}
+
           <Route
             path="/template"
             element={
@@ -28,6 +26,7 @@ function App() {
               </ProtectedRoute>
             }
           />
+
           <Route
             path="/profile"
             element={
@@ -36,6 +35,7 @@ function App() {
               </ProtectedRoute>
             }
           />
+
           <Route
             path="/templates/:id"
             element={
@@ -44,9 +44,10 @@ function App() {
               </ProtectedRoute>
             }
           />
+
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/enable-mfa" element={<EnableMFA />} />
+
           <Route
             path="/upload"
             element={
@@ -55,7 +56,7 @@ function App() {
               </ProtectedRoute>
             }
           />
-          \
+
           <Route
             path="/result"
             element={
